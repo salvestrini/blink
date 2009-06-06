2dc2e02d4ac07075d1894db3b539815b05c21449
Francesco Salvestrini <salvestrini@gmail.com>
2009-06-06 15:34:14 +0200

    Added site related files

---
+++ site/in/index.scm
@@ -0,0 +1,32 @@
+(h2 "DESCRIPTION")
+(p "Blink is a boot-time linker.")
+
+(h2 "COPYING")
+(p "The project is licensed under the "
+   (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
+      "GNU General Public License, version 2"))
+
+(h2 "MAINTAINERS")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
+
+(h2 "AUTHORS")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
+
+(h2 "MAILING LISTS")
+(p "The project has a single moderated mailing list, with an archive. "
+   "In order to post a message to the mailing list you must be subscribed. "
+   "Please consult the "
+   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/blink-generic"))
+      "mailing list page")
+   " for more information on subscribing to the mailing list.")
+
+(h2 "REPORT A BUG")
+(p "If you think you have found a bug then please send as complete a report "
+   "as possible to "
+   "<blink-generic AT nongnu DOT org>. "
+   "An easy way to collect all the required information, such as platform and "
+   "compiler, is to include in your report the config.log file available at "
+   "the end of the configuration procedure.")
+(p "If you have a patch for a bug that hasn't yet been fixed in the "
+   "latest repository sources, please be so kind to create it using the "
+   "repository sources, not the release sources.")
---
+++ site/in/site.as
@@ -0,0 +1,14 @@
+autogen definitions sitetool;
+
+common = {
+	layout = "./layout.sxml";
+};
+
+vars = {
+	var-sitename = "ELKLib";
+};
+
+include "contents.as"
+include "pages.as"
+include "map.as"
+include "files.as"
